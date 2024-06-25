01702584ab92f6124a40dbaa5f44a34a3e17f449
Saeed Ghorbani <ghorbanisaeed82@gmail.com>
2024-06-26 02:13:16 +0330

    importing flask sql alchemy

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import *
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
@@ -7,4 +9,4 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000,debug=True)
