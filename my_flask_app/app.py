420b34a9b2b3707ba1dacd38090c7c901994efcf
Saeed-54996 <ghorbanisaeed82@gmail.com>
2024-06-26 17:41:50 +0330

    adding mysql database configuration to the project

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,12 +1,17 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
-
+from db_config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)
+    app.run(port=5000, debug=True)
