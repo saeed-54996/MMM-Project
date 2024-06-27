831440360c28cf5f89980b18aea5026d2a08f5a0
Saeed Ghorbani <ghorbanisaeed82@gmail.com>
2024-06-27 22:49:11 +0330

    updating migrations

--- my_flask_app/model.py
+++ my_flask_app/model.py
@@ -1,20 +1,28 @@
 from init import db
 
 class Users(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=True)
+    # Relationship to Articles
+    articles = db.relationship('Articles', backref='user', lazy=True)
+    # Relationship to Images
+    images = db.relationship('Images', backref='user', lazy=True)
 
 class Articles(db.Model):
+    __tablename__ = 'articles'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(120), nullable=True)
     content = db.Column(db.Text, nullable=False)
     category = db.Column(db.String(50), nullable=True)
     keywords = db.Column(db.Text, nullable=True)
+    # Foreign key linking to Users table
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 class Images(db.Model):
+    __tablename__ = 'images'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(50), nullable=False)
     path = db.Column(db.Text, nullable=False)
@@ -24,7 +32,8 @@ class Images(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
 class Categories(db.Model):
+    __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
     type = db.Column(db.String(50), nullable=False)
-    is_deleted = db.Column(db.Integer, default=0) 
+    is_deleted = db.Column(db.Integer, default=0)
